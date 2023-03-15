d18800a43915649a237d4b7b36d1e457cdc25272
hker9527 <owo@uwu.cc>
2023-03-15 10:44:47 +0800

    Update

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Client, Events, GatewayIntentBits, GuildScheduledEventCreateOptions } from "discord.js";
+import { Client, Events, GatewayIntentBits, GuildScheduledEventCreateOptions, SlashCommandBooleanOption, SlashCommandBuilder, SlashCommandIntegerOption } from "discord.js";
 import { getData } from "./repositories";
 import sharp from "sharp";
 import dotenv from "dotenv";
@@ -12,16 +12,17 @@ const client = new Client({
     ]
 });
 
-client.on(Events.ClientReady, async () => {
-    console.log("Ready");
-    const guilds = client.guilds.cache;
-    
-    for (const [_, guild] of guilds) {
-        const data = await getData();
+let days = 7;
 
+const updateEvents = async () => {
+    const guilds = client.guilds.cache;
+
+    const data = await getData();
+
+    for (const [_, guild] of guilds) {
         // Find items which birthday is in 1 week
         const now = new Date();
-        const week = 1000 * 60 * 60 * 24 * 7;
+        const week = 1000 * 60 * 60 * 24 * days;
 
         const items = data.filter(({ birthday }) => {
             const diff = +birthday - +now;
@@ -44,23 +45,71 @@ client.on(Events.ClientReady, async () => {
                 }
             };
             const image = Buffer.from(await (await fetch(item.img)).arrayBuffer());
-                const buffer = await sharp(image).resize({
-                    width: 1250,
-                    height: 500,
-                    fit: "cover",
-                    position: "left"
-                }).toBuffer();
+            const buffer = await sharp(image).resize({
+                width: 1250,
+                height: 500,
+                fit: "cover",
+                position: "left"
+            }).toBuffer();
             options.image = buffer;
-            
+
             const event = events.find(event => event.name === options.name);
             if (!event) {
                 await guild.scheduledEvents.create(options);
             }
         }
     }
+};
 
-    client.destroy();
-    process.exit(0);
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    switch (interaction.commandName) {
+        case "update":
+            days = interaction.options.getInteger("days") ?? days;
+            await interaction.deferReply();
+            await updateEvents();
+            await interaction.editReply({
+                content: "Updated birthday for " + days + " days."
+            });
+            return;
+        case "auto":
+            const enabled = interaction.options.getBoolean("enabled", true);
+            if (enabled) {
+                setInterval(updateEvents, 86400 * 1000);
+            }
+            await interaction.reply("Set enabled to " + enabled);
+            return;
+    }
+});
+
+client.on(Events.ClientReady, async () => {
+    console.log("Ready");
+
+    const commands = [
+        new SlashCommandBuilder()
+            .setName("update")
+            .setDescription("Updates the birthday.")
+            .addIntegerOption(
+                new SlashCommandIntegerOption()
+                    .setMinValue(1)
+                    .setMaxValue(365)
+                    .setRequired(false)
+                    .setName("days")
+                    .setDescription("How many days to update, default 1 week")
+            ),
+        new SlashCommandBuilder()
+            .setName("auto")
+            .setDescription("Set auto-update of birthday daily.")
+            .addBooleanOption(
+                new SlashCommandBooleanOption()
+                    .setRequired(true)
+                    .setName("enabled")
+                    .setDescription("Enable?")
+            )
+    ]
+
+    await client.application!.commands.set(commands);
 });
 
 client.login(process.env.TOKEN);
